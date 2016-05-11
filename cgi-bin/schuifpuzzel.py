dd828a2505e5f744be74f565076aaf858eb7e9cd
Pieter De Clercq <pieterdeclercq@outlook.com>
2016-05-11 20:15:33 +0200

    Merged api

--- cgi-bin/schuifpuzzel.py
+++ cgi-bin/schuifpuzzel.py
@@ -2,9 +2,13 @@
 
 import cgi
 import json
+import hashlib
 import os.path
 import urllib.parse
 import urllib.request
+import cgitb
+
+cgitb.enable()
 
 from PIL import Image, ImageOps
 
@@ -17,6 +21,10 @@ def lastfm(method: str, a: str) -> dict:
     return json.loads(urllib.request.urlopen(urllib.request.Request(
         "http://ws.audioscrobbler.com/2.0/?method={}&format=json&artist={}&api_key=c94f6bae579e47179e8fbde12880a85c".format(
             method, a))).read().decode("utf8"))
+
+
+def hash(s: str) -> str:
+    return hashlib.md5(s).hexdigest()
 
 
 def parseartist(a: str) -> str:
@@ -36,20 +44,20 @@ def ok(f: str, c: int, r: int, ts: tuple):
 
 
 print("Access-Control-Allow-Origin: *")
-print("Content-Type: application/json\n")
+print("Content-Type: text/html\n")
 
 sizes = ['thumbnail', 'small', 'medium', 'large', 'extralarge', 'mega']
 
 artist = cgi.FieldStorage().getvalue("artiest").lower()
-cleanArtist = parseartist(artist)
+artisthash = hash(artist.encode("utf-8"))
 rows = int(cgi.FieldStorage().getvalue("rijen"))
 cols = int(cgi.FieldStorage().getvalue("kolommen"))
 
-artistFolder = "assets/puzzles/{}".format(cleanArtist)
+artistFolder = "assets/puzzles/{}".format(artisthash)
 folder = "{}/{}x{}".format(artistFolder, str(cols), str(rows))
 
 if os.path.exists(folder):
-    ok(folder, cols, rows, Image.open("{}/0_0.png".format(folder)).size)
+    ok(folder, cols, rows, Image.open("{}/1.png".format(folder)).size)
 
 if not os.path.isfile("{}/original.png".format(artistFolder)):
     albums = lastfm('artist.search', artist).get('results')
@@ -58,8 +66,7 @@ if not os.path.isfile("{}/original.png".format(artistFolder)):
         respond(False, "No albums found.")
     results = {sizes.index(album['size']): album['#text'] for album in albums[0].get('image')}
 
-    if not os.path.exists(artistFolder):
-        os.makedirs(artistFolder)
+    os.makedirs(artistFolder)
 
     img = Image.open(download(results[max(results)])[0])
 
@@ -79,7 +86,7 @@ tileSize = borderedImg.size[0] // cols, borderedImg.size[1] // rows
 os.makedirs(folder)
 
 [borderedImg.copy().crop((c * tileSize[0], r * tileSize[1], (c + 1) * tileSize[0], (r + 1) * tileSize
-[1])).save("{}/{}_{}.{}".format(folder, c, r, 'png')) for c in range(0, cols)
+[1])).save("{}/{}.{}".format(folder, r * rows + c + 1, 'png')) for c in range(0, cols)
  for r in range(0, rows)]
 
 ok(folder, cols, rows, tileSize)
